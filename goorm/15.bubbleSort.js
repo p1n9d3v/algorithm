4cae7a57c184952ea709b0a20db9609f7086724b
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2024-01-22 16:38:33 +0900

    feat(goorm): 2024-01-22

---
+++ goorm/15.bubbleSort.js
@@ -0,0 +1,31 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+rl.on("line", function (line) {
+    input.push(line.split(" ").map(Number));
+}).on("close", function () {
+    const [N, arr] = input;
+    solve(arr);
+    console.log(arr.join(" "));
+    process.exit();
+});
+
+function solve(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+}
+
+function swap(arr, index1, index2) {
+    const temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
