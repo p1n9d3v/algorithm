7c91560b0d99864de6ee482e516044b5e4a69f4e
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2024-05-22 19:10:01 +0900

    feat(utils): Add Comparator class

---
+++ utils/Comparator.js
@@ -0,0 +1,36 @@
+export default class Comparator {
+    constructor(compareFunction) {
+        this.compare = compareFunction || Comparator.defaultCompareFunction;
+    }
+
+    static defaultCompareFunction(a, b) {
+        if (a === b) return 0;
+
+        return a < b ? -1 : 1;
+    }
+
+    euqal(a, b) {
+        return this.compare(a, b) === 0;
+    }
+
+    lessThan(a, b) {
+        return this.compare(a, b) < 0;
+    }
+
+    greaterThan(a, b) {
+        return this.compare(a, b) > 0;
+    }
+
+    lessThanOrEqual(a, b) {
+        return this.lessThan(a, b) || this.euqal(a, b);
+    }
+
+    greaterThanOrEqual(a, b) {
+        return this.greaterThan(a, b) || this.euqal(a, b);
+    }
+
+    reverse() {
+        const compareOriginal = this.compare;
+        this.compare = (a, b) => compareOriginal(b, a);
+    }
+}
